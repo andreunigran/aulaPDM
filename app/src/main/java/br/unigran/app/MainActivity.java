c5930af01938a452bc0b8c43d219225ec8f0c0d2
andre <andremnascimento@ufgd.edu.br>
2020-08-14 20:03:39 -0400

    Exemplo AlertDialog

--- app/src/main/java/br/unigran/app/MainActivity.java
+++ app/src/main/java/br/unigran/app/MainActivity.java
@@ -1,5 +1,7 @@
 package br.unigran.app;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         //capitura do xml
         botao=findViewById(R.id.buttonOk);
         campoTexto=findViewById(R.id.etnome);
-
         //modelo ação
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +32,28 @@ public class MainActivity extends AppCompatActivity {
     //modelo açao
     public void acaobotao(View view){
         Toast.makeText(getApplicationContext(),"Olá "+campoTexto.getText(),Toast.LENGTH_LONG).show();
+    }
+    public void alerta(View view){
+        //cria o builder
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);//sem estilo
+        //AlertDialog.Builder alerta = new AlertDialog.Builder(this,R.style.Theme_AppCompat_NoActionBar);//com estilo
+        alerta.setMessage("Alerta de teste")
+       // .create().show();//poderia ser msm linha
+        ;
+
+        alerta.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),"Olá "+campoTexto.getText(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+        alerta.setNegativeButton("Cancekar",null);
+
+
+        //crio o dialog
+        AlertDialog dialog =alerta.create();
+       dialog.show();// chamo a msg
+
     }
 }
